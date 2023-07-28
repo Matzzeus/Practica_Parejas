b2c7aeb616318d408a244fdebec27749b325c8fd
Mario Ajpop <matzul2027@gmail.com>
2023-07-28 17:29:12 -0600

    First commit

---
+++ src/main/java/com/mycompany/tarea1/Tarea1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.tarea1;
+
+
+public class Tarea1 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Hello World!");
+        
+        
+    }
+}
